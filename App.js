10d077f36a1ec3ce52c4a7a9e01383e75b348572
Fernando José <fernando.jose.santos@hotmail.com>
2020-06-13 20:20:45 -0300

    Configuração das fontes de texto

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//importar screens melhora o procesamento nativo
+import { enableScreens } from 'react-native-screens';
+
+//importar as fontes e dar um expo install expo-font
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+enableScreens();
+
+//importando a pilha de navigation
+//import MealsNavigator from './navigation/MealsNavigator';
+
+//adicionar função para buscar as fontes personalizadas
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+    />;
+  }
+  // return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
