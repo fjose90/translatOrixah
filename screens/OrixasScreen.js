7c981753c2241ea1e9f28f14728351c19e2cb279
Fernando José <fernando.jose.santos@hotmail.com>
2020-06-14 11:10:08 -0300

    grid items separando em colunas

--- screens/OrixasScreen.js
+++ screens/OrixasScreen.js
@@ -3,26 +3,38 @@ import {
     StyleSheet,
     Text,
     View,
-    Button
+    FlatList
 } from 'react-native';
+
+import { ORIXA } from '../data/dummy-data';
+
+//renderizar os nomes dos orixás
+const renderGridItem = (NameData) => {
+    return <View style={styles.gridItem}>
+        <Text>{NameData.item.name}</Text>
+    </View>
+};
 
 const OrixasScreen = props => {
     return (
-        <View style={styles.screen}>
-            <Text>TELA DE OrixasScreen</Text>
-            <Button
-                title="Ir para Orixas Musics"
-                onPress={() => {
-                    props.navigation.navigate({ routeName: 'OrixaMusics' });
-                }} />
-        </View>);
+        <FlatList
+            keyExtractor={(item, index) => item.id}
+            data={ORIXA}
+            numColumns={2}
+            renderItem={renderGridItem}
+
+        />
+
+    );
 };
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }
-
+    },
+    gridItem: {
+        flex: 1
+    },
 });
 export default OrixasScreen;
