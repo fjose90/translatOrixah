9a9c92b0a34918ffc1a22bf9fd50817e3051b035
Fernando José <fernando.jose.santos@hotmail.com>
2020-06-17 18:50:20 -0300

    snipers

--- App.js
+++ App.js
@@ -1,41 +1,43 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 //importar screens melhora o procesamento nativo
-import { enableScreens } from 'react-native-screens';
+import { enableScreens } from "react-native-screens";
 
 //importar as fontes e dar um expo install expo-font
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
 enableScreens();
 
 //importando a pilha de navigator
-import OrixasNavigator from './navigation/OrixasNavigator';
+import OrixasNavigator from "./navigation/OrixasNavigator";
 
 //adicionar função para buscar as fontes personalizadas
 const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
+    return Font.loadAsync({
+        "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+        "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    });
 };
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-  if (!fontLoaded) {
-    return <AppLoading
-      startAsync={fetchFonts}
-      onFinish={() => setFontLoaded(true)}
-    />;
-  }
-  return <OrixasNavigator />;
+    const [fontLoaded, setFontLoaded] = useState(false);
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setFontLoaded(true)}
+            />
+        );
+    }
+    return <OrixasNavigator />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
