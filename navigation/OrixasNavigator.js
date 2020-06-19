9a07d5de2cad936f1397d04f12e5e97e8bb4b4a9
Fernando José <fernando.jose.santos@hotmail.com>
2020-06-19 17:40:44 -0300

    petrier

--- navigation/OrixasNavigator.js
+++ navigation/OrixasNavigator.js
@@ -1,45 +1,48 @@
-
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { Platform } from 'react-native';
-import Colors from '../constants/Colors';
+import { Platform } from "react-native";
+import Colors from "../constants/Colors";
 
-import OrixasScreen from '../screens/OrixasScreen';
-import OrixaMusicsScreen from '../screens/OrixaMusicsScreen';
-import MusicScreen from '../screens/MusicScreen';
+import OrixasScreen from "../screens/OrixasScreen";
+import OrixaMusicsScreen from "../screens/OrixaMusicsScreen";
+import MusicScreen from "../screens/MusicScreen";
 
-const OrixasNavigator = createStackNavigator({
-    Orixas: {
-        screen: OrixasScreen,
-    },
-
-    OrixaMusics: {
-        screen: OrixaMusicsScreen,
-        navigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : Colors.primaryColor
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : 'white'
-        }
-    },
-
-    Music: {
-        screen: MusicScreen,
-    }
-
-},
+const OrixasNavigator = createStackNavigator(
     {
-        // initialRouteName: 'Categories',//configura tela inicial 
-        //mode: 'modal',//para transição personalizada entre telas
+        Orixas: {
+            screen: OrixasScreen,
+        },
+
+        OrixaMusics: {
+            screen: OrixaMusicsScreen,
+            navigationOptions: {
+                headerStyle: {
+                    backgroundColor:
+                        Platform.OS === "android"
+                            ? Colors.primaryColor
+                            : Colors.primaryColor,
+                },
+                headerTintColor: Platform.OS === "android" ? "white" : "white",
+            },
+        },
+
+        Music: {
+            screen: MusicScreen,
+        },
+    },
+    {
+        initialRouteName: "Orixas", //configura tela inicial
+        //mode: "modal", //para transição personalizada entre telas
         defaultNavigationOptions: {
             headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : Colors.primaryColor
+                backgroundColor:
+                    Platform.OS === "android"
+                        ? Colors.primaryColor
+                        : Colors.primaryColor,
             },
-            headerTintColor: Platform.OS === 'android' ? 'white' : 'white'
-        }
+            headerTintColor: Platform.OS === "android" ? "white" : "white",
+        },
     }
-
-
 );
 
 export default createAppContainer(OrixasNavigator);
