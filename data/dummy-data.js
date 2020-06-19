2fbab27c7f6053713df9e824121c508101e6bf66
Fernando José <fernando.jose.santos@hotmail.com>
2020-06-19 16:23:14 -0300

    Add data das Musicas

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,4 +1,5 @@
 import Orixa from "../models/Orixa";
+import Music from "../models/Music";
 
 export const ORIXAS = [
     new Orixa("o1", "Italian", "#f5428d"),
@@ -11,4 +12,16 @@ export const ORIXAS = [
     new Orixa("o8", "Asian", "#b9ffb0"),
     new Orixa("o9", "French", "#ffc7ff"),
     new Orixa("o10", "Hamburgers", "#9eecff"),
+];
+export const MUSICS = [
+    new Music("m1", "o1", "music1", "/imgCover", "/fileMusic"),
+    new Music("m2", "o1", "music2", "/imgCover", "/fileMusic"),
+    new Music("m3", "o1", "music3", "/imgCover", "/fileMusic"),
+    new Music("m4", "o1", "music4", "/imgCover", "/fileMusic"),
+    new Music("m5", "o1", "music5", "/imgCover", "/fileMusic"),
+    new Music("m6", "o1", "music6", "/imgCover", "/fileMusic"),
+    new Music("m7", "o1", "music7", "/imgCover", "/fileMusic"),
+    new Music("m8", "o1", "music8", "/imgCover", "/fileMusic"),
+    new Music("m9", "o1", "music9", "/imgCover", "/fileMusic"),
+    new Music("m10", "o1", "music10", "/imgCover", "/fileMusic"),
 ];
