0b3808fb0ecd11c7429032d93212d0b18a186ffe
Fernando José <fernando.jose.santos@hotmail.com>
2020-07-27 18:21:22 -0300

    dimension desnessesário

--- screens/OrixasScreen.js
+++ screens/OrixasScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, FlatList, Dimensions } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import { ORIXAS } from "../data/dummy-data";
 import OrixaGridTile from "../components/OrixaGridTile";
 
